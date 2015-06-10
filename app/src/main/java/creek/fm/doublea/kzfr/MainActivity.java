f03a2e6315c1e7f1fe8a3a6d2f14c5fb8fcd0489
Aaron <amcintyre2@mail.csuchico.edu>
2015-06-09 23:05:44 -0700

    [Foundation] Add drawer and toolbar to main
    All activities will inherit from main so this commit adds the menu and
    drawer layout to the main activity.

--- app/src/main/java/creek/fm/doublea/kzfr/MainActivity.java
+++ app/src/main/java/creek/fm/doublea/kzfr/MainActivity.java
@@ -1,23 +1,61 @@
 package creek.fm.doublea.kzfr;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toolbar mainActionToolBar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainActionToolBar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            setupActionBar(actionBar);
+        }
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                        this,
+                        drawerLayout,
+                        R.string.open_drawer_content_description,
+                        R.string.close_drawer_content_description) {
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                return super.onOptionsItemSelected(item);
+                //TODO add actions here for the drawer menu
+            }
+        };
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    public void setupActionBar(ActionBar actionBar){
+        actionBar.setIcon(R.mipmap.kzfr_logo);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //TODO add share button to action bar in v2.0
         return true;
     }
 
@@ -28,7 +66,13 @@ public class MainActivity extends AppCompatActivity{
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
+        actionBarDrawerToggle.onOptionsItemSelected(item);
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
