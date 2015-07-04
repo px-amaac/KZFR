ffe13e922d868ca49835c678a058fc16d1280f03
Aaron <amcintyre2@mail.csuchico.edu>
2015-07-03 23:18:40 -0700

    [BugFix] Implement picasso palette transformation
    This file deserves recognition. This is the solution for using the
    awesome palette api released in the support library with picasso. The
    solution is discussed here:
    http://jakewharton.com/coercing-picasso-to-play-with-palette/
    Thanks for that much thought out post Jake. Very useful.

---
+++ app/src/main/java/creek/fm/doublea/kzfr/utils/PaletteTransformation.java
@@ -0,0 +1,41 @@
+package creek.fm.doublea.kzfr.utils;
+
+import android.graphics.Bitmap;
+import android.support.v7.graphics.Palette;
+
+import com.squareup.picasso.Transformation;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+/**
+ * Solution taken from a post by JakeWharton on using palette with picasso.
+ * http://jakewharton.com/coercing-picasso-to-play-with-palette/
+ */
+public final class PaletteTransformation implements Transformation{
+    private static final PaletteTransformation INSTANCE = new PaletteTransformation();
+    private static final Map<Bitmap, Palette> CACHE = new WeakHashMap<>();
+
+    public static PaletteTransformation instance() {
+        return INSTANCE;
+    }
+
+    public static Palette getPalette(Bitmap bitmap) {
+        return CACHE.get(bitmap);
+    }
+
+    private PaletteTransformation() {}
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+
+        Palette palette = Palette.generate(source);
+        CACHE.put(source, palette);
+        return source;
+    }
+
+    @Override
+    public String key() {
+        return "";
+    }
+}
