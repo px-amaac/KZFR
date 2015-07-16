661582938c24e16fd4beb1cbc8c6e8d82e3c0b6c
Aaron <amcintyre2@mail.csuchico.edu>
2015-07-16 00:22:27 -0700

    [UI] Fix text sizes
    Some of the text sizes were not looking good together. I made the
    program view text sizes 16dp instead of 18dp to match the rest of the
    app.

--- app/src/main/java/creek/fm/doublea/kzfr/services/NowPlayingService.java
+++ app/src/main/java/creek/fm/doublea/kzfr/services/NowPlayingService.java
@@ -39,7 +39,9 @@ public class NowPlayingService extends Service implements AudioManager.OnAudioFo
     private AudioManager mAudioManager = null;
 
     //The URL that feeds the KZFR stream.
-    private static final String mStreamUrl = "http://stream-tx1.radioparadise.com:8090/;stream/1";
+    //http://107.170.235.213:8081/prpfm-aac8/prpfm8/playlist.m3u8
+    //http://stream-tx1.radioparadise.com:8090/;stream/1
+    private static final String mStreamUrl = "http://107.170.235.213:8081/prpfm-aac8/prpfm8/playlist.m3u8";
 
     //Wifi Lock to ensure the wifo does not ge to sleep while we are stearming music.
     WifiManager.WifiLock mWifiLock;
